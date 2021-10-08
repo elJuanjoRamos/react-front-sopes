903db3d98101b948bbd226ad89cf4ada9278383a
Juan Jose Ramos Campos <eljuanjo.ramos@gmail.com>
2021-10-08 01:11:19 -0600

    Update Reportes.jsx

--- src/pages/Reportes/Reportes.jsx
+++ src/pages/Reportes/Reportes.jsx
@@ -14,7 +14,7 @@ const Reportes = () => {
     const [dates, setDates] = useState([])
     const [tweets, setTweets] = useState([])
 
-    var socket = openSocket('http://localhost:4200');
+    var socket = openSocket('http://34.71.233.178:4200');
     useEffect(()=> {
         setTimeout(() => {
             socket.emit('getNoticias');
